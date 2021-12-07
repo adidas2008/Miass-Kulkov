b645cc7f4b2a01c14dfb1b87289d68e08986b632
adidas2008 <94910926+adidas2008@users.noreply.github.com>
2021-12-07 17:58:37 +0500

    hello

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print('My first git program')
+    print('And I changed it')
+    print('Create new branch')
+
+
+def superfunction():
+    print('Hello world')
+
+
+if __name__ == '__main__':
